941b6ade67452e503390b0047a84c26ea3655970
Curtis Schiewek <curtis.schiewek@gmail.com>
2017-07-31 18:51:18 -0400

    add all queires

--- lib/mix/tasks/benchmark.ex
+++ lib/mix/tasks/benchmark.ex
@@ -2,16 +2,47 @@ defmodule Mix.Tasks.Benchmark do
   use Mix.Task
   @shortdoc "Runs the benchmarks"
 
-  def run(_) do
-    {:ok, results} = """
-    {
-      foos(count: 1001) {
+  @all_fields """
+    query($count: Int!) {
+      foos(count: $count) {
+        id
+        int1
+        int2
+        string1
+        string2
+        boolean1
+        boolean2
+        stringArray
+        intArray
+        booleanArray
+      }
+    }
+  """
+
+  @no_arrays """
+    query($count: Int!) {
+      foos(count: $count) {
+        id
+        int1
+        int2
+        string1
+        string2
+        boolean1
+        boolean2
+      }
+    }
+  """
+
+  @only_id """
+    query($count: Int!) {
+      foos(count: $count) {
         id
       }
     }
-    """
-    |> Absinthe.run(Schema)
+  """
 
-    IO.puts inspect(results)
+  def run(_) do
+    {:ok, results} = Absinthe.run(@all_fields, Schema, variables: %{"count" => 1000})
+    IO.puts results
   end
 end
